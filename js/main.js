cb8ff7fbd4e2c6769d6f3b66693f0f57d7efd224
Apoorva Mandikal <apoorvagirish76@gmail.com>
2022-02-28 21:19:52 +0530

    Functionalities and Layouts done

--- js/main.js
+++ js/main.js
@@ -12,8 +12,52 @@ function getPics(searchText){
     axios.get('https://api.nasa.gov/planetary/apod?api_key='+apiKey+'&date='+searchText)
     .then((response)=>{
         console.log(response);
+        let pictures = response.data;
+        let output = '';
+        output = `
+                    <div class ="well text-center">
+                        <h5>${pictures.date}</h5>
+                        <div class="bg-image hover-overlay ripple shadow-1-strong rounded" data-ripple-color="light">
+                            <a onclick="pictureSelected('${pictures.date}')" href="#" >
+                            <img src = '${pictures.hdurl}'>
+                            </a>
+                        </div>
+                    </div>
+            `;
+        $('#picture').html(output);
     })
     .catch((err)=> {
         console.log(err);
+    });
+}
+
+function pictureSelected(date){
+    sessionStorage.setItem('picDate', date);
+    window.location = 'picture.html';
+    return false;
+}
+
+function getPicture(){
+    let picDate = sessionStorage.getItem('picDate');
+
+    axios.get('https://api.nasa.gov/planetary/apod?api_key='+apiKey+'&date='+picDate)
+    .then((response)=>{
+        console.log(response);
+        let picture = response.data;
+        $('#intro-date').html(picture.date);
+        $('#title').html(picture.title)
+        let output = '';
+        output = `
+                    <div class ="well text-center">
+                        <img src = '${picture.hdurl}'>
+                    </div>
+            `;
+        $('#pictureExp').html(output);
+        $('#copyright').html(picture.copyright);
+        $('#explanation').html(picture.explanation);
+
     })
+    .catch((err)=> {
+        console.log(err);
+    });
 }

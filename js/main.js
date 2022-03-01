b0b6a16fdef4d4b1405a5cb2f33c9f413c5f8870
Apoorva Mandikal <apoorvagirish76@gmail.com>
2022-03-02 00:02:07 +0530

    Complete functionality done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 let apiKey = config.apiKey;
 
+
 $(document).ready(()=> {
+    $("#searchText").datepicker({
+        format: 'yyyy-mm-dd',
+        startDate: '1995-06-16',
+        endDate: '+0d',
+        autoclose: true
+    }).on("changeDate", function (e) {
+        $("#searchForm").submit();
+    });
     $('#searchForm').on('submit', (e) => {
         let searchText = $('#searchText').val();
         getPics(searchText);
@@ -13,8 +22,9 @@ function getPics(searchText){
     .then((response)=>{
         console.log(response);
         let pictures = response.data;
+        let media_type = pictures.media_type;
         let output = '';
-        output = `
+        picoutput = `
                     <div class ="well text-center">
                         <h5>${pictures.date}</h5>
                         <div class="bg-image hover-overlay ripple shadow-1-strong rounded" data-ripple-color="light">
@@ -24,7 +34,28 @@ function getPics(searchText){
                         </div>
                     </div>
             `;
-        $('#picture').html(output);
+        vidoutput = `
+                    <div class ="well text-center">
+                        <h5>${pictures.date}</h5>
+                        <div>
+                            <iframe
+                                class="shadow-1-strong rounded"
+                                src='${pictures.url}'
+                                title="YouTube video"
+                                height="500" width="1000"
+                                allowfullscreen>
+                            </iframe>
+                            <p class="text-muted mb-2">
+                                Explanation: 
+                            </p>
+                            <p>${pictures.explanation}</p>
+                        </div>
+                    </div>
+            `;
+        if(media_type == "image" ){
+            $('#picture').html(picoutput);
+        }
+        else $('#picture').html(vidoutput);
     })
     .catch((err)=> {
         console.log(err);
